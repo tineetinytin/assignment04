7b89df8dc433def0131518f898b239c70922f83d
tineetinytin <christine.elene@gmail.com>
2021-03-05 15:42:37 -0800

    added modules

---
+++ js/modules/calculator.js
@@ -0,0 +1,19 @@
+const add = (x, y) => {
+    let sum = x + y;
+    return `The sum of two numbers is ${sum}.`;
+};
+
+const subtract = (x, y) => {
+    let diff = x - y;
+    return `The difference of two numbers is ${diff}.`;
+};
+
+const multiply = (x, y) => {
+    let mult = x * y;
+    return `The product of two numbers is ${mult}.`;
+};
+
+const divide = (x, y) => {
+    let divd = x / y;
+    return `The quotient of two numbers is ${divd}.`;
+};
--- js/script.js
+++ js/script.js
@@ -25,48 +25,62 @@
 //ROCK, PAPER, SCISSORS
 
 
-let user = prompt("Do you choose rock, paper or scissors?");
-let choice = Math.random();
-    if (choice < 0.34) {
-	    choice = 'rock';
-    } else if(choice <= 0.67) {
-	    choice = 'paper';
-    } else {
-	    choice = 'scissors';
-    }
-let compare = function(x, y) {
+// let user = prompt("Do you choose rock, paper or scissors?");
+// let choice = Math.random();
+//     if (choice < 0.34) {
+// 	    choice = 'rock';
+//     } else if(choice <= 0.67) {
+// 	    choice = 'paper';
+//     } else {
+// 	    choice = 'scissors';
+//     }
+// let compare = function(x, y) {
 
-    if(x === y){
-        return "The result is a tie!";
+//     if(x === y){
+//         return "The result is a tie!";
 
-    } else if (x === 'rock') {
+//     } else if (x === 'rock') {
 
-        if (y ===' scissors') {
-            return 'rock wins'
+//         if (y ===' scissors') {
+//             return 'rock wins'
 
-        } else if (y === 'paper') {
-            return 'paper wins';
-        }
+//         } else if (y === 'paper') {
+//             return 'paper wins';
+//         }
 
-    } else if (x === 'paper') {
+//     } else if (x === 'paper') {
 
-        if(y === 'scissors') {
-            return 'scissors win'
+//         if(y === 'scissors') {
+//             return 'scissors win'
 
-        } else if(y === 'rock') {
-            return 'paper wins';
-        }
-    } else if(x === 'scissors') {
-        if(y === 'paper') {
-            return "scissors win"
+//         } else if(y === 'rock') {
+//             return 'paper wins';
+//         }
+//     } else if(x === 'scissors') {
+//         if(y === 'paper') {
+//             return "scissors win"
 
-        } else if(y === 'rock') {
-            return "rock wins";
-        }
-    }
+//         } else if(y === 'rock') {
+//             return "rock wins";
+//         }
+//     }
     
-}
-console.log(compare(user,choice));
+// }
+// console.log(compare(user,choice));
 
 //BASIC CALCULATOR
+import * as calc from './modules/calulator.js';
+console.log(calc.add(5, 5));
+
+// let x = Number(prompt('Give me a number'));
+// let y = Number(prompt('Give me a number'));
+// let operation = prompt('What operation would you like to perform?(add, subtract, multiply, divide)');
+
+
+
+
+
+
+
+
 
