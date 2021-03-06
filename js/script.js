b31872c0c28b81566ad8f60e12283e5d29a64d36
tineetinytin <christine.elene@gmail.com>
2021-03-05 17:16:35 -0800

    dd

--- js/script.js
+++ js/script.js
@@ -24,73 +24,84 @@
 
 //ROCK, PAPER, SCISSORS
 
+function start() {
 
-// let user = prompt("Do you choose rock, paper or scissors?");
-// let choice = Math.random();
-//     if (choice < 0.34) {
-// 	    choice = 'rock';
-//     } else if(choice <= 0.67) {
-// 	    choice = 'paper';
-//     } else {
-// 	    choice = 'scissors';
-//     }
-// let compare = function(x, y) {
 
-//     if(x === y){
-//         return "The result is a tie!";
+let user = prompt("Do you choose rock, paper or scissors?");
+let choice = Math.random();
+    if (choice < 0.34) {
+	    choice = 'rock';
+    } else if(choice <= 0.67) {
+	    choice = 'paper';
+    } else {
+	    choice = 'scissors';
+    }
+let compare = function(x, y) {
 
-//     } else if (x === 'rock') {
+    if(x === y){
+        return alert('The result is a tie!');
 
-//         if (y ===' scissors') {
-//             return 'rock wins'
+    } else if (x === 'rock') {
 
-//         } else if (y === 'paper') {
-//             return 'paper wins';
-//         }
+        if (y ===' scissors') {
+            return alert('rock wins');
 
-//     } else if (x === 'paper') {
+        } else if (y === 'paper') {
+            return alert('paper wins');
+        }
 
-//         if(y === 'scissors') {
-//             return 'scissors win'
+    } else if (x === 'paper') {
 
-//         } else if(y === 'rock') {
-//             return 'paper wins';
-//         }
-//     } else if(x === 'scissors') {
-//         if(y === 'paper') {
-//             return "scissors win"
+        if(y === 'scissors') {
+            return alert('scissors win');
 
-//         } else if(y === 'rock') {
-//             return "rock wins";
-//         }
-//     }
+        } else if(y === 'rock') {
+            return alert('paper wins');
+        }
+    } else if(x === 'scissors') {
+        if(y === 'paper') {
+            return alert('scissors win');
+
+        } else if(y === 'rock') {
+            return alert('rock wins');
+        }
+    }
     
-// }
-// console.log(compare(user,choice));
+}
+console.log(compare(user,choice));
+start();
+}
+
+
 
 //BASIC CALCULATOR
-import * as calc from './modules/calculator.js';
+// import * as calc from './modules/calculator.js';
 
-function start() {
+// function start() {
     
-    while (true) {
-        let x = Number(prompt('Give me a number'));
-        let y = Number(prompt('Give me a number'));
+//     while (true) {
+//         let x = Number(prompt('Give me a number'));
+//         let y = Number(prompt('Give me a number'));
         
-        let operation = prompt('What operation would you like to perform?(add, subtract, multiply, divide)'); 
+//         let operation = prompt('What operation would you like to perform?(add, subtract, multiply, divide)'); 
   
         
-        if (operation === 'add') {
-            alert(calc.add(x,y));
-        } else if (operation === 'subtract') {
-            alert(calc.subtract(x,y));
-        } else if (operation === 'multiply') {
-            alert(calc.multiply(x,y));
-        } else if (operation === 'divide') {
-            alert(calc.divide(x,y));}
-    }    
-}
-start();
+//         if (operation === 'add') {
+//             alert(calc.add(x,y));
+//         } else if (operation === 'subtract') {
+//             alert(calc.subtract(x,y));
+//         } else if (operation === 'multiply') {
+//             alert(calc.multiply(x,y));
+//         } else if (operation === 'divide') {
+//             alert(calc.divide(x,y));
+//         } else {
+//             alert( 'Please choose one add, subtract, multiply or divide');
+//             start();
+//         }
+        
+//     }    
+// }
+// start();
 
 
 
